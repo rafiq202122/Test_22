45903def072fa736648cbc7b61348ef3e2e5f399
rafiq202122 <rafiqulislam4362@gmail.com>
2025-01-08 19:46:46 +0600

    div adding

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   return (
     <div>
       <h1 className='bg-red-400 tex-7xl font-bold'> Hellow World</h1>
+      <div className='h-6 w-8 bg-red-950'>
+
+      </div>
     </div>
   )
 }
