962b1bb181957ba6867ee533e2b201530241ef70
rafiq202122 <rafiqulislam4362@gmail.com>
2025-01-08 19:44:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-400 tex-7xl font-bold'> Hellow World</h1>
+    </div>
+  )
+}
+
+export default App
